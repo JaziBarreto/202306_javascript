f4a4b3c26e7b81bd19fc392a69bc2da1c89e8b26
JaziBarreto <jazminbarreto19@gmail.com>
2023-07-01 16:38:10 -0400

    dojonary

---
+++ 0. dojonary/script.js
@@ -0,0 +1,19 @@
+function logOut(element) {
+    element.innerText = "Logout";
+    return logOut;
+}
+logOut();
+
+function addDefinition(element) {
+    element.remove();
+    return logOut;
+}
+addDefinition();
+
+function showLikedMessage() {
+    alert("Ninja was liked!");
+}
+showLikedMessage ();
+
+
+
