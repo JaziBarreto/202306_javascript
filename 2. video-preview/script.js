490a70dc117c8d42a9f66d53c0193f1ec930b1cd
JaziBarreto <jazminbarreto19@gmail.com>
2023-07-01 12:30:07 -0400

    reviewed

--- 2. video-preview/script.js
+++ 2. video-preview/script.js
@@ -1,11 +1,9 @@
 function playVideo() {
     var video = document.getElementById("new-video");
-    video.muted = false;
     video.play();
 }
 
 function pauseVideo() {
     var video = document.getElementById("new-video");
-    video.muted = true;
     video.pause();
 }
